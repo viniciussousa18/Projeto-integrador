1b86ec74caf260f9c9ca3d5427dbcbaf24b4fc2b
Técnico em Informática 2019.6 <16121742019.6@senacrjedu.local>
2020-02-10 11:32:17 -0300

    alterei a pagina conexao

--- treineconosco.php
+++ treineconosco.php
@@ -14,7 +14,7 @@
 </head>
 
 <body>
-    <b>
+<form action = "gravar.php" method="post">
     <nav class="navbar navbar-expand-lg navbar-light bg-light"><a class="navbar-brand" href="index.html"><img src="img/logo.jfif"></a>  
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				  <span class="navbar-toggler-icon"></span>
